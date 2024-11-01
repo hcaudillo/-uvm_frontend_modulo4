39a7468e9f9ccdb4d3089755ec3582e4ed1fe9bb
Hugo Caudillo <hugo.caudillo@cidesi.edu.mx>
2024-10-31 21:38:31 -0600

    Practica 3, se corrigio el package.json mediante el npm init y quitando el type: module

---
+++ Practica_3/app.js
@@ -0,0 +1,71 @@
+/*import { version, command as _command, argv } from 'yargs'*/
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+yargs.version('1.1.0')
+
+// Crea Comando Agregar
+yargs.command({
+    command: 'Agregar',
+    describe: 'Agrega una nueva nota',
+    builder: {
+        title: {
+            describe: 'Titulo de la Nota',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Cuerpo de la Nota',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+
+// Crea comando borrar
+yargs.command({
+    command: 'Borrar',
+    describe: 'Borrar una nota',
+    builder: {
+        title: {
+            describe: 'Titulo de la Nota',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// Crea comando lisstar
+yargs.command({
+    command: 'Listar',
+    describe: 'Lista las notas',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// Crea comando leer
+yargs.command({
+    command: 'Leer',
+    describe: 'Lee una nota',
+    builder: {
+        title: {
+            describe: 'Titulo de la Nota',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse();
