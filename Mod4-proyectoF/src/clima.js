c5bf7db28f01bb7bff2aa72888ec881702198ad5
Hugo Caudillo <hugo.caudillo@cidesi.edu.mx>
2024-11-13 09:40:16 -0600

    Cambios en img de html y prueba de captura de errores en climaJS

--- Mod4-proyectoF/src/clima.js
+++ Mod4-proyectoF/src/clima.js
@@ -16,37 +16,35 @@ async function getClima() {
             method: 'GET', 
             headers: headers,  
         });
-
-        if (response.status === 200) {
-            const data = await response.json();
-            
-            if(data.country != null || data.country != undefined){
-                seccion.style.display = "block";
+            if (response.status === 200) {
+                const data = await response.json();
                 
-                icono.src = data.imagenClima;
-                donde.innerHTML = "El clima de " + data.name +' - ' + data.country ;
-                climaResponse.innerHTML = ` Clima:              ${data.descripcionClima} <br><br>
-                                            Temp. Actual(C):    ${data.temperaturaActual} <br>
-                                            Sensacion (C):      ${data.sensacion} <br>
-                                            Humedad:            ${data.humedad} <br><br>
-                                            Indice UV:          ${data.indiceUv} <br><br>
-                                            Direccion del Viento:       ${data.direccionViento} <br>
-                                            Velocidad del viento(KPH):  ${data.velocidadViento} <br><br>
-                                            Visibilidad (KM):   ${data.visibilidad} <br>
-                                            Hora local:         ${data.horaLocal} <br>
-                                           `
-            }else {
-                icono.src = "";
-                donde.innerHTML = "Pais no Encontrado...";
-                climaResponse.innerHTML = "";
+                if(data.country != null || data.country != undefined){
+                    seccion.style.display = "block";
+                    
+                    icono.src = data.imagenClima;
+                    donde.innerHTML = "El clima de " + data.name +' - ' + data.country ;
+                    climaResponse.innerHTML = ` Clima:              ${data.descripcionClima} <br><br>
+                                                Temp. Actual(C):    ${data.temperaturaActual} <br>
+                                                Sensacion (C):      ${data.sensacion} <br>
+                                                Humedad:            ${data.humedad} <br><br>
+                                                Indice UV:          ${data.indiceUv} <br><br>
+                                                Direccion del Viento:       ${data.direccionViento} <br>
+                                                Velocidad del viento(KPH):  ${data.velocidadViento} <br><br>
+                                                Visibilidad (KM):   ${data.visibilidad} <br>
+                                                Hora local:         ${data.horaLocal} <br>
+                                               `
+                }else {
+                    icono.src = "";
+                    donde.innerHTML = "Pais no Encontrado...";
+                    climaResponse.innerHTML = "";
+                }
+            } else {
+                seccion.style.display = "none";
+                if (response.status === 400) this.errors = ['Invalid app_permissions value. (err.400)'];
+                if (response.status === 401) this.errors = ['Acces denied. (err.401)'];
             }
-
-
-        } else {
-            seccion.style.display = "none";
-            if (response.status === 400) this.errors = ['Invalid app_permissions value. (err.400)'];
-            if (response.status === 401) this.errors = ['Acces denied. (err.401)'];
-        }
+    
     } catch (error) {
         console.log(error);
         this.errors = ["Se disparo un error al momento de recuperar la información desde la Url."]
